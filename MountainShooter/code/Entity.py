4bde3678ddc6ec657f852521168caf9328096d73
Douglas Souza <douglassouza033@gmail.com>
2024-07-07 21:32:36 -0300

    AULA 3 - crição de tiro e colisão de entidades

--- MountainShooter/code/Entity.py
+++ MountainShooter/code/Entity.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 import pygame.image
 
+from code.constants import ENTITY_SPEED, ENTITY_HEALTH
+
 
 class Entity(ABC):
 
@@ -9,7 +11,8 @@ class Entity(ABC):
         self.nome = name
         self.surf = pygame.image.load('./Asset/' + name + '.png').convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
-        self.speed = 0
+        self.speed = ENTITY_SPEED[self.nome]
+        self.health = ENTITY_HEALTH[self.nome]
 
     @abstractmethod
     def move(self):
--- MountainShooter/code/Player.py
+++ MountainShooter/code/Player.py
@@ -1,24 +1,38 @@
 import pygame
 
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 from code.constants import WIN_HEIGHT, ENTITY_SPEED, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
-    PLAYER_KEY_RIGHT
+    PLAYER_KEY_RIGHT, PLAYER_KEY_SHOOT, ENTITY_SHOT_DELAY
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.name = name
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def move(self):
         pressed_key = pygame.key.get_pressed()
-        if pressed_key[PLAYER_KEY_UP[self.nome]] and self.rect.top > 0:
-            self.rect.centery -= ENTITY_SPEED[self.nome]
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:
+            self.rect.centery -= ENTITY_SPEED[self.name]
 
-        if pressed_key[PLAYER_KEY_DOWN[self.nome]] and self.rect.bottom < WIN_HEIGHT:
-            self.rect.centery += ENTITY_SPEED[self.nome]
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:
+            self.rect.centery += ENTITY_SPEED[self.name]
 
-        if pressed_key[PLAYER_KEY_LEFT[self.nome]] and self.rect.left > 0:
-            self.rect.centerx -= ENTITY_SPEED[self.nome]
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
 
-        if pressed_key[PLAYER_KEY_RIGHT[self.nome]] and self.rect.left < WIN_WIDTH-59:
-            self.rect.centerx += ENTITY_SPEED[self.nome]
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.left < WIN_WIDTH - 59:
+            self.rect.centerx += ENTITY_SPEED[self.name]
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
+                return PlayerShot(f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+        else:
+            return None
+
